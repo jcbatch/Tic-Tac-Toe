05f3b26b0e484f93f8140ac90727520216b756d3
James Batchik <jcbatch@umich.edu>
2016-04-15 01:37:19 -0400

    added board files

---
+++ Board.cpp
@@ -0,0 +1,11 @@
+//
+//  Boad.cpp
+//  Tic Tac Toe
+//
+//  Created by James Batchik on 4/14/16.
+//  Copyright © 2016 James Batchik. All rights reserved.
+//
+
+#include "Board.h"
+
+
---
+++ Board.h
@@ -0,0 +1,25 @@
+//
+//  Boad.hpp
+//  Tic Tac Toe
+//
+//  Created by James Batchik on 4/14/16.
+//  Copyright © 2016 James Batchik. All rights reserved.
+//
+#ifndef Board_h
+#define Board_h
+
+class Board {
+public:
+    Board();
+    
+    
+private:
+    
+    
+};
+
+class PlayerPos {
+
+};
+
+#endif
